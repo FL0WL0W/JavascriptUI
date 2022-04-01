0923fc84a9577467269e14b9e342ce197ea70346
FL0WL0W <FL0WL0W@Cracked-Ice.com>
2022-03-31 22:25:57 -0500

    allow multiple dialogs to be open

--- UIDialog.js
+++ UIDialog.js
@@ -38,9 +38,10 @@ export default class UIDialog extends HTMLDivElement {
         super();
         this.title = `Dialog`;
         this.buttonLabel - `Open`;
-        this.class = `ui dialog`;
         this.append(this.#button);
-        this.append(this.#dialog);
+        this.#dialog.class = `ui dialog`;
+        this.#dialog.style.bottom = `0px`;
+        document.getElementsByTagName(`body`)[0].append(this.#dialog);
         this.#titleBarElement.class = `titlebar`;
         this.#dialog.append(this.#titleBarElement);
         this.content.class = `content`
@@ -52,7 +53,7 @@ export default class UIDialog extends HTMLDivElement {
         Object.assign(this, prop);
         const thisClass = this;
         this.#button.addEventListener(`click`, function() {
-            thisClass.#dialog.showModal();
+            thisClass.#dialog.show();
             thisClass.#dialog.style.top = `0px`;
             thisClass.#dialog.style.left = `0px`;
         });
@@ -62,17 +63,22 @@ export default class UIDialog extends HTMLDivElement {
             delete thisClass.#dialog.style.left;
         })
         this.#titleBarElement.addEventListener(`mousedown`, function(event) {
+            thisClass.#dialog.style.left = thisClass.#dialog.offsetLeft;
+            thisClass.#dialog.style.right = `auto`;
+            thisClass.#dialog.style.top = thisClass.#dialog.offsetTop;
+            thisClass.#dialog.style.bottom = `auto`;
             let state = {
                 pageX: event.pageX,
-                pageY: event.pageY
+                pageY: event.pageY,
+                left: parseFloat(thisClass.#dialog.style.left),
+                top: parseFloat(thisClass.#dialog.style.top)
             }
-            function mouseMove(event) {
-                let xDiff = event.pageX - state.pageX;
-                let yDiff = event.pageY - state.pageY;
-                thisClass.#dialog.style.left = parseFloat(thisClass.#dialog.style.left) + (xDiff*2) + `px`;
-                thisClass.#dialog.style.top = parseFloat(thisClass.#dialog.style.top) + (yDiff*2) + `px`;
-                state.pageX += xDiff;
-                state.pageY += yDiff;
+            function mouseMove(mevent) {
+                let xDiff = mevent.pageX - state.pageX;
+                let yDiff = mevent.pageY - state.pageY;
+                thisClass.#dialog.style.left = state.left + xDiff;
+                thisClass.#dialog.style.top = state.top + yDiff;
+                console.log(thisClass.#dialog.style.left);
             }
             function mouseUp() {
                 document.removeEventListener(`mousemove`, mouseMove);
