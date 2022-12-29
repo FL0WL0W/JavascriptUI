cae9760bde7aaf49bbaec08549064250c7a7a114
FL0WL0W <FL0WL0W@Cracked-Ice.com>
2022-12-28 22:32:09 -0600

    add UITextArea

---
+++ UITextArea.js
@@ -0,0 +1,30 @@
+export default class UITextArea extends HTMLTextAreaElement {
+    #value = ``
+    get value() {
+        return this.#value
+    }
+    set value(value) {
+        if(this.#value === value)
+            return
+
+        super.value = this.#value = value
+        super.dispatchEvent(new Event(`change`, {bubbles: true}))
+    }
+
+    get saveValue() {
+        return this.value
+    }
+    set saveValue(saveValue){
+        this.value = saveValue
+    }
+
+    constructor(prop) {
+        super()
+        this.class = `ui text`
+        this.addEventListener(`change`, () => {
+            this.#value = super.value
+        })
+        Object.assign(this, prop)
+    }
+}
+customElements.define(`ui-textarea`, UITextArea, { extends: `textarea` })
